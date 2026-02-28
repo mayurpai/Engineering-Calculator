bb499c817f4ecbb9b9d1718b621d06db1a8ba1b0
Mayur Pai <mayur5pai@gmail.com>
2026-03-01 00:20:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,9 @@ export const metadata: Metadata = {
   authors: [{ name: "Mayur Pai" }],
   creator: "Mayur Pai",
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
-    apple: "/favicon.svg",
+    icon: "/Engineering-Calculator/favicon.svg",
+    shortcut: "/Engineering-Calculator/favicon.svg",
+    apple: "/Engineering-Calculator/favicon.svg",
   },
 };
 
